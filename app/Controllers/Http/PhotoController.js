4a64f08bed9f3d770eb32645c8730f9ba2be9fc2
ignjas <ignas.jasonas@gmail.com>
2020-01-05 17:00:55 +0200

    Add image upload endpoint

--- app/Controllers/Http/PhotoController.js
+++ app/Controllers/Http/PhotoController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const Photo = use("App/Models/Photo");
+const Helpers = use('Helpers')
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -15,9 +16,23 @@ class PhotoController {
    * @param {Response} ctx.response
    */
   async store({ request, response }) {
-    const photoData = request.only(["name"]);
-    const photo = await Photo.create(photoData);
-    return photo.id;
+    const profilePic = request.file("photo", {
+      types: ["image"],
+      size: "2mb"
+    });
+
+    const photo = await Photo.create({});
+
+    await profilePic.move(Helpers.tmpPath("uploads"), {
+      name: `${photo.id}.jpg`,
+      overwrite: true
+    });
+
+    if (!profilePic.moved()) {
+      return profilePic.error();
+    }
+
+    response.status(200).send({ id: photo.id });
   }
 }
 
